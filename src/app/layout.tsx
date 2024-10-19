c2e293477586914ce1c2157197a66ba292fc0727
Ibrokhim Ismoilov <user@Ibrokhims-MacBook-Pro.local>
2024-10-19 14:55:33 +0500

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,29 @@ import QueryClientProvider from "@/providers/query-client"
 import { UserAuthProvider } from "@/providers/user-auth/user-auth-provider"
 
 export const metadata: Metadata = {
-  title: "Neruda.uz",
-  description: "",
+  metadataBase: new URL("https://neruda.uz"),
+  title: "Neruda",
+  description: "Neruda",
+  authors: [
+    {
+      name: "Ibrokhim Ismoilov",
+      url: "https://neruda.uz",
+    },
+  ],
+  icons: { icon: "/favicon.png" },
+  keywords:
+    "Tog' jinsi toshlari, Tosh konlari savdosi,Konchilik toshlari,Klinets sotib olish,Ezilgan toshlar,Qurilish toshlari,Tosh qazib olish,Minerallar va toshlar,Tabiiy toshlar,Qimmatbaho toshlar,Sheben, Klinets, Uzbekiston sheben, Toshkent sheben, Uzbekiston klinets,Toshkent klinets,Драгоценные камни,Продажа горных камней,Минеральные камни,Покупка клинкера,Дробленый камень,Камни для строительства,Горные минералы,Природные камни,Продажа клинца и камней,Драгоценные и дробленые камни, Камни Ташкент,Ташкент шебень,Ташкент клинкец",
+  openGraph: {
+    title: "Neruda.uz",
+    description: "Ibrokhim Ismoilov",
+    type: "website",
+    url: "https://neruda.uz",
+    locale: "uz_UZ",
+    images: "/mock-ielts.png",
+    countryName: "Uzbekistan",
+    siteName: "Neruda",
+    emails: "hello@neruda.uz",
+  },
 }
 
 const inter = Inter({ subsets: ["cyrillic"] })
